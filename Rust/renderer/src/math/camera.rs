3e4f7857f7d38e3fdf151552aa2842d717a76acd
danielvisser333 <danielvisser10@outlook.com>
2022-02-09 19:00:24 +0100

    Fix camera

--- Rust/renderer/src/math/camera.rs
+++ Rust/renderer/src/math/camera.rs
@@ -53,22 +53,29 @@ impl Camera{
     pub fn correct_perspective(&mut self, extent : Extent2D){
         self.aspect = extent.width as f32 / extent.height as f32;
         self.projection = cgmath::perspective(Deg(self.fov), self.aspect, self.near, self.far);
+        self.projection[1][1] = self.projection[1][1] * -1.0;
     }
     pub fn update(&mut self){
         self.matrix.matrix=self.projection*self.view*self.model;
     }
     pub fn mouse_movement(&mut self, delta : (f64,f64)){
         if self.left_mouse_button_pressed{
-            let sin_x = cgmath::Angle::sin(Deg(delta.0 as f32));
-            let cos_x = cgmath::Angle::cos(Deg(delta.0 as f32));
-            let quat_alpha = Quaternion::new(0.0, self.eye.x-self.center.x, self.eye.y-self.center.y, self.eye.z-self.center.z).normalize();
-
+            let sin_x = cgmath::Angle::sin(Deg(delta.0 as f32/2.0));
+            let cos_x = cgmath::Angle::cos(Deg(delta.0 as f32/2.0));
+            let quat_alpha = Quaternion::new(0.0, self.up.x - self.center.x, self.up.y - self.center.y, self.up.z - self.center.z).normalize();
+            let mut quat_q = sin_x * quat_alpha;
+            quat_q.s += cos_x;
+            let quat_beta = Quaternion::new(0.0, self.eye.x-self.center.x, self.eye.y-self.center.y, self.eye.z-self.center.z);
+            let new_eye = (quat_q*quat_beta*quat_q.conjugate()).v;
+            self.eye = Point3::new(new_eye.x+self.center.x, new_eye.y+self.center.y, new_eye.z+self.center.z);
+            self.view = Matrix4::look_at_rh(self.eye,self.center, self.up);
         }
     }
     pub fn mouse_zoom(&mut self, delta : f32){
         if self.fov + delta/100.0 < 180.0 && self.fov + delta/100.0 > 0.0{
             self.fov += delta/100.0;
             self.projection = cgmath::perspective(Deg(self.fov), self.aspect, self.near, self.far); 
+            self.projection[1][1] = self.projection[1][1] * -1.0;
         };
     }
 }
